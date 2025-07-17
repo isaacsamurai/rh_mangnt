a1cfa5529fd979016127a6ee3752af82428e6c31
Isaac <isaacsamuel002@gmail.com>
2025-07-16 21:01:04 -0300

    creating  migrations and seeds

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Mail\Message;
 
-Route::get('/', function () {
-    echo "RH Mangnt";
+Route::get('/email', function () {
+
+    Mail::raw('Teste de email!', function (Message $message) {
+        $message->to('teste@gmail.com')
+            ->subject('Bem-vindo ao RH MANGNT!')
+            ->from('rh@rhmangnt.com');
+    });
+
+    echo 'Email enviado com sucesso!';
 });
